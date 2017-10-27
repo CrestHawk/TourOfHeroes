b0f82dcb104c007b48dc847bcf573f6817d13681
Thomas Davies <emailtomdavies@gmail.com>
2017-10-27 16:27:16 +0100

    tour of heroes intro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  //template: `<h1>{{title}}</h1> <h2>{{hero}} details!</h2>`,
+  templateUrl: "./views/MainAppComponent.html"
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  
+{ 
+    title: string;
+    myHero: string;
+    heroes: Array<string>;
+
+    constructor()
+    {
+        this.title = "Marvel vs. Capcom 2: New Age of Heroes";
+        this.heroes = ["Spiderman", "Wolverine", "Magneto"]
+        this.myHero = this.heroes[0];
+    }
+}
+
