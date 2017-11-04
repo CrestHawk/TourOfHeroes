2122ac760f2ab81aff30707c1d0047155eda57f9
CrestHawk <emailtomdavies@gmail.com>
2017-11-04 16:02:47 +0000

    First Tutorial Complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Hero } from './Instances/Hero';
 
 @Component({
   selector: 'my-app',
@@ -8,13 +9,16 @@ import { Component } from '@angular/core';
 export class AppComponent  
 { 
     title: string;
-    myHero: string;
-    heroes: Array<string>;
+    myHero: Hero;
+    heroes: Array<Hero>;
 
     constructor()
     {
         this.title = "Marvel vs. Capcom 2: New Age of Heroes";
-        this.heroes = ["Spiderman", "Wolverine", "Magneto"]
+        this.heroes = [ new Hero(1, "Spiderman"),
+                        new Hero(2, "Wolverine"),
+                        new Hero(3, "Magneto"),
+                        new Hero(4, "Megaman")];
         this.myHero = this.heroes[0];
     }
 }
