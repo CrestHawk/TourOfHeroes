d290b0fdcbe2dca47efbcb243307d606d5ec739e
CrestHawk <emailtomdavies@gmail.com>
2017-11-07 21:17:24 +0000

    Hero styling added and master/detail sub view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,22 +4,38 @@ import { Hero } from './Instances/Hero';
 @Component({
   selector: 'my-app',
   //template: `<h1>{{title}}</h1> <h2>{{hero}} details!</h2>`,
-  templateUrl: "./views/MainAppComponent.html"
+  templateUrl: "./views/MainAppComponent.html",
+  styleUrls: ['./styles/hero-app.component.css']
 })
 export class AppComponent  
 { 
     title: string;
     myHero: Hero;
     heroes: Array<Hero>;
+    selectedHero: Hero;
 
     constructor()
     {
         this.title = "Marvel vs. Capcom 2: New Age of Heroes";
-        this.heroes = [ new Hero(1, "Spiderman"),
-                        new Hero(2, "Wolverine"),
-                        new Hero(3, "Magneto"),
-                        new Hero(4, "Megaman")];
-        this.myHero = this.heroes[0];
+        const HEROES: Hero[] = [
+            { id: 11, name: 'Wolverine' },
+            { id: 12, name: 'Spiderman' },
+            { id: 13, name: 'Batman' },
+            { id: 14, name: 'Aqua Man' },
+            { id: 15, name: 'Captain Merica!' },
+            { id: 16, name: 'Luffy D. Monkey' },
+            { id: 17, name: 'Usopp' },
+            { id: 18, name: 'Brooke' },
+            { id: 19, name: 'Zolo' },
+            { id: 20, name: 'Sanji' } 
+        ];
+        this.heroes = HEROES;
+        this.myHero = this.heroes[1];
+    }
+
+    onSelect(hero: Hero): void
+    {
+        this.selectedHero = hero;
     }
 }
 
